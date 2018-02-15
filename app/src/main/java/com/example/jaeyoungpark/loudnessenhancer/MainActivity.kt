065ca5fd76a12dbf9e3fbf0570b0c31fb80207be
kujyp <pjo901018@korea.ac.kr>
2018-02-15 21:37:57 +0900

    Fix that loopBetween doesn't work

--- app/src/main/java/com/example/jaeyoungpark/loudnessenhancer/MainActivity.kt
+++ app/src/main/java/com/example/jaeyoungpark/loudnessenhancer/MainActivity.kt
@@ -13,43 +13,42 @@ import com.google.android.exoplayer2.source.TrackGroupArray
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import java.io.File
+import java.util.*
+import java.util.concurrent.TimeUnit
+import java.util.logging.LogManager
 
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.java.simpleName
 
     private lateinit var mPlayer: SimpleExoPlayer
     private var mLoopStartNEnd: Pair<Long, Long> = Pair(-1, -1)
+    private var mDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val uri: Uri = getUri()
+
+
+        val uri: Uri = getUri().also { Log.d(TAG, "onCreate() called  with: it = [$it]") }
         initPlayer(uri)
-        loopBetween(0, 3)
+        loopBetween(3000, 5000)
     }
 
     private fun loopBetween(start: Long, end: Long) {
+        mPlayer.seekTo(start)
         mLoopStartNEnd = Pair(start, end)
-    }
-
-    private fun notifyCurrentPosition(currentPosition: Long) {
-        Log.d(TAG, "notifyCurrentPosition() called  with: currentPosition = [$currentPosition]")
-
-        if (mLoopStartNEnd.first != (-1).toLong() && mLoopStartNEnd.second != (-1).toLong()) {
-            if (mLoopStartNEnd.second < currentPosition) {
-                mPlayer.seekTo(mLoopStartNEnd.first)
-            }
-
-        }
     }
 
     private fun getUri(): Uri {
         Log.d(TAG, "getUri: ")
         val dir: File = Environment.getExternalStoragePublicDirectory("Download")
         dir.listFiles().asIterable()
+//                .sortedByDescending { Random().nextInt() }
                 .forEach {
                     if (it.extension == "m4a") {
                         return Uri.fromFile(it)
@@ -73,34 +72,49 @@ class MainActivity : AppCompatActivity() {
 
         mPlayer.prepare(mediaSource, true, false)
 
+        mDisposable.add(Observable.interval(100, TimeUnit.MILLISECONDS)
+                .map { mPlayer }
+                .filter { !it.isLoading }
+                .map { it.to(mLoopStartNEnd) }
+                .filter { (player, loopStartNEnd) -> loopStartNEnd.first != (-1).toLong() && loopStartNEnd.second != (-1).toLong() }
+                .filter { (player, loopStartNEnd) -> loopStartNEnd.second < player.currentPosition }
+                .doOnNext { Log.d(TAG, "initPlayer() called it=$it") }
+                .subscribe { (player, loopStartNEnd) -> player.seekTo(loopStartNEnd.first) })
+
         mPlayer.addListener(object: ExoPlayer.EventListener {
             override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters?) {
+                Log.d(TAG, "onPlaybackParametersChanged: ")
 
             }
 
             override fun onTracksChanged(trackGroups: TrackGroupArray?, trackSelections: TrackSelectionArray?) {
+                Log.d(TAG, "onTracksChanged: ")
 
             }
 
             override fun onPlayerError(error: ExoPlaybackException?) {
+                Log.d(TAG, "onPlayerError: ")
 
             }
 
             override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                Log.d(TAG, "onPlayerStateChanged() called  with: playWhenReady = [$playWhenReady], playbackState = [$playbackState]")
+
 
             }
 
             override fun onLoadingChanged(isLoading: Boolean) {
+                Log.d(TAG, "onLoadingChanged() called  with: isLoading = [$isLoading]")
 
             }
 
             override fun onPositionDiscontinuity() {
                 Log.d(TAG, "onPositionDiscontinuity: ")
 
-                notifyCurrentPosition(mPlayer.currentPosition)
             }
 
             override fun onTimelineChanged(timeline: Timeline?, manifest: Any?) {
+                Log.d(TAG, "onTimelineChanged: ")
 
             }
         })
