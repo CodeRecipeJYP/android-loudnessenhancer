2336e05c7424d7593f794b686f39b0efe2fdb645
kujyp <pjo901018@korea.ac.kr>
2018-02-15 21:04:38 +0900

    Implement loopBetween

--- app/src/main/java/com/example/jaeyoungpark/loudnessenhancer/MainActivity.kt
+++ app/src/main/java/com/example/jaeyoungpark/loudnessenhancer/MainActivity.kt
@@ -6,13 +6,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
-import com.google.android.exoplayer2.DefaultLoadControl
-import com.google.android.exoplayer2.DefaultRenderersFactory
-import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
 import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.TrackGroupArray
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import java.io.File
 
@@ -20,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.java.simpleName
 
     private lateinit var mPlayer: SimpleExoPlayer
+    private var mLoopStartNEnd: Pair<Long, Long> = Pair(-1, -1)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +28,22 @@ class MainActivity : AppCompatActivity() {
 
         val uri: Uri = getUri()
         initPlayer(uri)
+        loopBetween(0, 3)
+    }
+
+    private fun loopBetween(start: Long, end: Long) {
+        mLoopStartNEnd = Pair(start, end)
+    }
+
+    private fun notifyCurrentPosition(currentPosition: Long) {
+        Log.d(TAG, "notifyCurrentPosition() called  with: currentPosition = [$currentPosition]")
+
+        if (mLoopStartNEnd.first != (-1).toLong() && mLoopStartNEnd.second != (-1).toLong()) {
+            if (mLoopStartNEnd.second < currentPosition) {
+                mPlayer.seekTo(mLoopStartNEnd.first)
+            }
+
+        }
     }
 
     private fun getUri(): Uri {
@@ -56,5 +72,37 @@ class MainActivity : AppCompatActivity() {
                 DefaultExtractorsFactory(), null, null)
 
         mPlayer.prepare(mediaSource, true, false)
+
+        mPlayer.addListener(object: ExoPlayer.EventListener {
+            override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters?) {
+
+            }
+
+            override fun onTracksChanged(trackGroups: TrackGroupArray?, trackSelections: TrackSelectionArray?) {
+
+            }
+
+            override fun onPlayerError(error: ExoPlaybackException?) {
+
+            }
+
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+
+            }
+
+            override fun onLoadingChanged(isLoading: Boolean) {
+
+            }
+
+            override fun onPositionDiscontinuity() {
+                Log.d(TAG, "onPositionDiscontinuity: ")
+
+                notifyCurrentPosition(mPlayer.currentPosition)
+            }
+
+            override fun onTimelineChanged(timeline: Timeline?, manifest: Any?) {
+
+            }
+        })
     }
 }
