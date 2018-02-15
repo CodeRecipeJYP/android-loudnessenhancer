a1d25dc62c0a69c69210123b535ee8283cc28791
kujyp <pjo901018@korea.ac.kr>
2018-02-15 22:41:34 +0900

    Add LoudnessEnhancer

--- app/src/main/java/com/example/jaeyoungpark/loudnessenhancer/MainActivity.kt
+++ app/src/main/java/com/example/jaeyoungpark/loudnessenhancer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jaeyoungpark.loudnessenhancer
 
+import android.media.audiofx.LoudnessEnhancer
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 
@@ -7,6 +8,8 @@ import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.audio.AudioRendererEventListener
+import com.google.android.exoplayer2.decoder.DecoderCounters
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.source.TrackGroupArray
@@ -15,16 +18,18 @@ import com.google.android.exoplayer2.trackselection.TrackSelectionArray
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
-import java.util.*
 import java.util.concurrent.TimeUnit
-import java.util.logging.LogManager
 
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.java.simpleName
 
     private lateinit var mPlayer: SimpleExoPlayer
     private var mLoopStartNEnd: Pair<Long, Long> = Pair(-1, -1)
+    private var mLoudnessEnhancer: LoudnessEnhancer? = null
+    private var mAudioSessionId: Int? = null
+
     private var mDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -79,7 +84,10 @@ class MainActivity : AppCompatActivity() {
                 .filter { (player, loopStartNEnd) -> loopStartNEnd.first != (-1).toLong() && loopStartNEnd.second != (-1).toLong() }
                 .filter { (player, loopStartNEnd) -> loopStartNEnd.second < player.currentPosition }
                 .doOnNext { Log.d(TAG, "initPlayer() called it=$it") }
-                .subscribe { (player, loopStartNEnd) -> player.seekTo(loopStartNEnd.first) })
+                .subscribe {
+                    (player, loopStartNEnd) -> player.seekTo(loopStartNEnd.first)
+                    toggleLoudnessEnhancer()
+                })
 
         mPlayer.addListener(object: ExoPlayer.EventListener {
             override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters?) {
@@ -100,7 +108,6 @@ class MainActivity : AppCompatActivity() {
             override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
                 Log.d(TAG, "onPlayerStateChanged() called  with: playWhenReady = [$playWhenReady], playbackState = [$playbackState]")
 
-
             }
 
             override fun onLoadingChanged(isLoading: Boolean) {
@@ -118,5 +125,81 @@ class MainActivity : AppCompatActivity() {
 
             }
         })
+
+        mPlayer.setAudioDebugListener(object: AudioRendererEventListener {
+            override fun onAudioEnabled(counters: DecoderCounters?) {
+                Log.d(TAG, "onAudioEnabled: ")
+
+            }
+
+            override fun onAudioInputFormatChanged(format: Format?) {
+                Log.d(TAG, "onAudioInputFormatChanged: ")
+
+            }
+
+            override fun onAudioTrackUnderrun(bufferSize: Int, bufferSizeMs: Long, elapsedSinceLastFeedMs: Long) {
+                Log.d(TAG, "onAudioTrackUnderrun: ")
+
+            }
+
+            override fun onAudioSessionId(audioSessionId: Int) {
+                Log.d(TAG, "onAudioSessionId: ")
+
+                initAudioSessionId(audioSessionId)
+            }
+
+            override fun onAudioDecoderInitialized(decoderName: String?, initializedTimestampMs: Long, initializationDurationMs: Long) {
+                Log.d(TAG, "onAudioDecoderInitialized: ")
+
+            }
+
+            override fun onAudioDisabled(counters: DecoderCounters?) {
+                Log.d(TAG, "onAudioDisabled: ")
+
+            }
+
+        })
+    }
+
+
+    private fun initAudioSessionId(audioSessionId: Int) {
+        Log.d(TAG, "initAudioSessionId() called  with: audioSessionId = [$audioSessionId]")
+
+        mAudioSessionId = audioSessionId
+    }
+
+    private fun toggleLoudnessEnhancer() {
+        Log.d(TAG, "increaseLoudnessEnhancer: ")
+
+        if (mLoudnessEnhancer == null) {
+            loudnessEnhancerActivate(4000)
+            toggleButton.post { toggleButton.isEnabled = true }
+        } else {
+            loudnessEnhancerDeactivate()
+            toggleButton.post { toggleButton.isEnabled = false }
+        }
+        Log.d(TAG, "increaseLoudnessEnhancer: mLoudnessEnhancer.enabled=${mLoudnessEnhancer != null}")
+        Log.d(TAG, "increaseLoudnessEnhancer: targetGain=${mLoudnessEnhancer?.targetGain}")
+
+    }
+
+    private fun loudnessEnhancerDeactivate() {
+        Log.d(TAG, "loudnessEnhancerDeactivate: ")
+
+        mLoudnessEnhancer?.apply {
+            enabled = false
+        }
+        mLoudnessEnhancer = null
+    }
+
+    private fun loudnessEnhancerActivate(gainmB: Int) {
+        Log.d(TAG, "loudnessEnhancerActivate() called  with: gainmB = [$gainmB]")
+
+        mAudioSessionId?.let {
+            mLoudnessEnhancer = LoudnessEnhancer(it).apply {
+                setTargetGain(gainmB)
+                enabled = true
+            }
+        }
     }
 }
